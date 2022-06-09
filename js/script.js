aef6561701ba9e5ef62f8d38ccab0893060a6ec6
Daggy <Daggy.od@gmail.com>
2022-06-09 21:32:29 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+/* alert('hello pidarasu'); */
+
+/* const result = confirm("tu huilo?"); */
+/* console.log(result); */
+
+/* const answer = +prompt("Tebe est` 18 ebanat?", "18");
+console.log(answer + 5); */
+
+/* const answers = [];
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Фамилия?', '');
+answers[2] = prompt('Ебанат?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null)); */
+
+/* Интерполяция  */
+/* const category = 'toys'; */
+
+/* console.log(`https://someurl.com/${category}/5`); */ /* `https://someurl.com/${category}/5` работает только с косыми кавычками */
+
+/* const user = "Daggy"; */
+
+/* alert(`Привет, ${user}`); */
+
+/* ОПЕРАТОРЫ */
+
+console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+/* incr++; */ /* Оператор инкримента, увеличение на единицу. Если ++ ставим перед - форма записи префиксная. Если после постфиксной*/
+/* decr--;  *//* Оператор декримента, уменьшение на единицу */
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+/* Операторы равенства */
+
+console.log(2*4 === 8); /* == Сравнение === Срогое сравнение = присваивание */
+
+/* Логические операторы */
+
+/* && */ /* Оператор и */
+/* || */ /* Оператор или */
+
+/* const isChecked = true,
+      isClose = true;
+
+      console.log(isChecked && isClose); */
+
+/*       const isChecked = true,
+      isClose = false;
+
+      console.log(isChecked || isClose); */
+
+/*       Оператор отрицания !*/
+
+const isChecked = false,
+isClose = false;
+
+console.log(isChecked || !isClose);
+
+/* Порядок выполнения операторов */
+
